a4d9170d12b037a8abeda147af484f71d5cf49ec
alex-forsure <asouthworth05@gmail.com>
2026-03-16 16:41:40 +0000

    Added bot, simplistic value function, bot can make draw decision.

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# TO DO (QOL):
-    # Auto-sort player's hand
-
 import random
 from copy import copy
 
 
 SUITS = ["H", "C", "S", "D"]
-RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"]
+RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
 HAND_SIZES = {2:10, 3:7, 4:7, 5:6, 6:6}
 
 
@@ -22,6 +19,7 @@ class Card():
 
     def __repr__(self):
         return self.code
+
 
 
 def run(cds:list):
@@ -42,6 +40,7 @@ def run(cds:list):
     cards.remove(min_card)
     c = RANKS.index(min_card.rank)
     i = 1
+
     # Iterate over next ranks until K is reached or a suit-matching card is not found.
     while i < 13:
         next_found = False
@@ -53,6 +52,12 @@ def run(cds:list):
                 i += 1
         if not next_found:
             break
+
+    # Allow for A23 runs.
+    if min_card.rank == "2":
+        if cards[0].code == "A" + suit:
+            run.insert(0, cards.pop(0))
+
     # If all cards were used in forming the run, the given set was a valid run. Otherwise it was not.
     if cards == []:
         return run
@@ -117,34 +122,6 @@ class Run(Meld):
                 out += [card]
         self.next = out
     
-        
-
-
-# Class Meld
-# Meld object stores:
-    # Cards in meld
-    # Is set/run
-    # Viable additions
-
-# Cards store scoring player
-
-# Playing melds
-    # table calls on player to play melds
-    # player returns a list of meld objects with setup complete
-
-
-
-kh = Card("K", "H")
-kd = Card("K", "D")
-ks = Card("K", "S")
-qs = Card("Q", "S")
-js = Card("J", "S")
-jd = Card("J", "D")
-ts = Card("T", "S")
-eights = Card("8", "S")
-
-mylot = Lot([kh, kd, ks])
-print(mylot)
 
 
 
@@ -160,6 +137,9 @@ def deck():
 class Table():
     def __init__(self, players : list, hand_size : int = 0):
         self.players = players
+        for player in players:
+            player.table = self
+        self.hand_size = hand_size
         if hand_size == 0:
             try:
                 self.hand_size = HAND_SIZES[len(players)]
@@ -197,11 +177,7 @@ class Table():
         print("")
 
 
-    
-
-
     def play(self):
-
 
         # Set up stock, players' hands, discard pile
         self.stock = deck()
@@ -213,8 +189,6 @@ class Table():
             p.hand = hand
         self.discard = [self.stock.pop()]
 
-        self.players[0].hand = [Card("Q", "H"), Card("J", "H"), Card("T", "H"), Card("8", "H"), Card("7", "C"), Card("6", "C")]
-        self.players[1].hand = [Card("9", "H"), Card("K", "H"), Card("4", "C")]
         # Start game loop
         playing = True
         player_index = -1 # -1 so that player 0 starts
@@ -232,19 +206,18 @@ class Table():
             print("")
 
 
+            self.show_all()
             # First, player draws. Method player.draw() returns true if he wants to draw from the discard pile. Otherwise he draws from the stock.
             if cur_player.draw():
                 cur_player.hand.append(self.discard.pop())
             else:
                 cur_player.hand.append(self.stock.pop())
                 
-                    
             # Then, player forms melds.
             self.melds += cur_player.meld()
 
             # Next lay off.
             cur_player.lay_off(self.melds)
-            
             
             # Finally, player chooses a card to place on the discard pile.
             self.discard.append(cur_player.discard())
@@ -253,10 +226,10 @@ class Table():
       
 # Base class for CPU + human players
 class Player():
-    def __init__(self, name : str, hand : list = [], melds : list = []):
+    def __init__(self, name : str, hand : list = []):
         self.hand = hand
-        self.melds = melds
         self.name = name
+        self.table = None
     
 
     def show_hand(self):
@@ -288,10 +261,135 @@ class Player():
         print("Discarding")
 
 
+class Bot(Player):
+    def __init__(self, name : str, hand : list = []):
+        super().__init__(name, hand)
+    
+    def draw(self):
+        # We come up with a list of all cards that could be in the stock.
+        # Other player's cards are unknown but we know everything in the discard pile.
+        # This assumes we are playing with one deck!
+        stock = [c.code for c in deck()]
+        for c in self.table.discard:
+            stock.remove(c.code)
+        for meld in self.table.melds:
+            for c in meld.cards:
+                stock.remove(c.code)
+        for c in self.hand:
+            stock.remove(c.code)
+        
+        # We take EV of stock.
+        values = {}
+        exp_value = 0
+        for c in stock:
+            exp_value += value(Card(c[0], c[1]), self.hand, self.table.melds)
+        exp_value = exp_value/len(stock)
+
+        print(self.name + "'s hand: " + str(self.hand))
+        if value(self.table.discard[-1], self.hand, self.table.melds) >= exp_value:
+            print(self.name + " draws from discard. Expected value of stock was " + str(exp_value) + " and value of discard was " + str(value(self.table.discard[-1], self.hand, self.table.melds)) + ".")
+            return True
+
+        print(self.name + " draws from stock. Expected value of stock was " + str(exp_value) + " and value of discard was " + str(value(self.table.discard[-1], self.hand, self.table.melds)) + ".")
+        return False
+    
+
+    def meld(self):
+        return([])
+
+    def lay_off(self, melds):
+        pass
+    
+    def discard(self):
+        return(self.hand.pop())
+
+
+def value(card, hand, melds):
+    # First, if a card can immediately be laid off, its value is 1.
+    for meld in melds:
+        if lot(meld.cards + [card]) or run(meld.cards + [card]):
+            return 1
+
+    # Next, if a card can immediately be melded, its value is 1.
+    if len(meld_with(card, hand)) >= 3:
+        return 1
+    
+    # Finally, if a card pairs with another card, its value is 0.5.
+    if len(meld_with(card, hand)) == 2:
+        return 0.5 
+    
+    return 0
+
+
+# Builds the biggest run/lot that we can find which contains the specified card, and cards in hand.
+# Returns the biggest run/lot, prioritising runs.
+# Doesn't work properly for runs when card is A atm.
+def meld_with(card, hand): 
+    
+    # We use card codes because they're easier to work with.
+    codes = [c.code for c in hand]
+    lot_codes = [card.code]
+
+    # Built the biggest lot we can.
+    for suit in SUITS:
+        if card.rank + suit in codes:
+            lot_codes += [card.rank + suit]
+
+    # Set up for finding a run.
+    run_codes = [card.code]
+    suit = card.suit
+    index = RANKS.index(card.rank)
+    c = index
+    # Tick down from the specified card, adding in whatever is found.
+    while c >= 0: # -1 possibility allows for A23 runs
+        if RANKS[c-1] + suit in codes:
+            run_codes += [RANKS[c-1] + suit]
+            c -= 1
+        else:
+            break
+    
+    # Reset counter and tick up from the specified card, adding in whatever is found.
+    c = index
+    while c < 12:
+        if RANKS[c+1] + suit in codes:
+            run_codes += [RANKS[c+1] + suit]
+            c += 1
+        else:
+            break
+    
+    # Pick the largest meld, prioritising runs.
+    meld_codes = run_codes
+    if len(run_codes) < len(lot_codes):
+        meld_codes = lot_codes
+    
+    # Convert meld codes into card objects and return.
+    meld = [card]
+    for c in hand:
+        if c.code in meld_codes:
+            meld += [c]
+    return meld
+
+        
+kh = Card("K", "H")
+kd = Card("K", "D")
+ks = Card("K", "S")
+qs = Card("Q", "S")
+js = Card("J", "S")
+jd = Card("J", "D")
+ts = Card("T", "S")
+eights = Card("8", "S")
+
+iid = Card("2", "D")
+iiid = Card("3", "D")
+ivd = Card("4", "D")
+ad = Card("A", "D")
+
+
+
 
 class Human(Player):
-    def __init__(self, name : str, hand : list = [], melds : list = []):
-        super().__init__(name, hand, melds)
+    def __init__(self, name : str, hand : list = []):
+        super().__init__(name, hand)
 
     # Choose to draw from either stock or discard. True = discard, False = stock.
     def draw(self):
@@ -406,7 +504,8 @@ class Human(Player):
 
 player1 = Human("Player 1")
 player2 = Human("Player 2")
-my_table = Table([player1, player2])
+bot = Bot("Wall-E")
+my_table = Table([bot, player1], hand_size=7)
 
 my_table.play()
 
